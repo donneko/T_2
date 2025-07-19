a1b1565bb0cfa72de6bd196d67fe8e8bb98c010a
komekome12 <ludouzhongze5@gmail.com>
2025-07-19 22:27:32 +0900

    jsの変更

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-        // // ここからボタン取得
-        // const end = document.getElementById("end")
+        //  ここからボタン取得
+        const eend = document.getElementById("end")
         const returnn = document.getElementById("return")
         const answer = document.getElementById("answer")
         const next = document.getElementById("next")
-        // const menu = document.getElementById("menu")
+        const menu = document.getElementById("menu")
 
-        // // ここから表示メッセージなどの取得
-        // const title = document.getElementById("title")
+        //  ここから表示メッセージなどの取得
+        const title = document.getElementById("title")
         const problem = document.getElementById("problem")
         const nu = document.getElementById("nu")
 
-        // //ここからユーザーの入力を取得
+        // ここからユーザーの入力を取得
         const user_input = document.getElementById("user-input")
 
         // 問題設定用のオブジェクト
         const setting = {
-            max: 10,
-            min: -10,
-            query: 10000,
+            max: 100,
+            min: -100,
+            query: 10,
             typee:2
         };
 
@@ -36,15 +36,22 @@
         let user_input_value = [];
         let user_answer = [];
 
-        console.log(setting.query)
+        //回答数
+        let conten = 0;
+
+        //ジャンル
+        let won_type = "数学";
+        let twe_type = "正負の数";
 
         set()
         // 初期設定（リストにデータを入れる）
         function set(){
+            document.title=(`${won_type}・${twe_type}`)
+            title.textContent=(`${won_type}・${twe_type}`)
+
             for(let i = 0; i < setting.query;i++){
                 user_input_value.push(null);
                 user_answer.push(false);
-                console.log(i)
             }
         }
         console.log(user_answer);
@@ -70,8 +77,7 @@
         console.log(`演算子:${operator}`)
         console.log(`演算結果:${answers}`)
 
-        //回答数
-        let conten = 0;
+
 
 
         update(conten);
@@ -106,18 +112,20 @@
 
         //ここからボタンの無効化
         //一度リセット
+        answer.style.color = ("var(--bu-invalid-color)");
+        answer.style.borderColor = ("var(--bu-invalid-color)");
         next.style.color = ("var(--text-color-next)");
         returnn.style.color = ("var(--text-color-main)");
-        next.style.bordercolor = ("var(--bu-next-color)");
+        next.style.borderColor = ("var(--bu-next-color)");
         returnn.style.borderColor = ("var(--bu-next-color)");
         next.style.backgroundColor = ("var(--bu-next-color)");
-
+        next.textContent = "次へ";
 
         if(!((conten +1)< setting.query)){
-            next.style.color = ("var(--bu-invalid-color)");
-            next.style.borderColor = ("var(--bu-invalid-color)");
+            next.style.color = ("var(--bu-end-color)");
+            next.style.borderColor = ("var(--bu-end-color)");
             next.style.backgroundColor = ("var(--bg-bu-color)");
-
+            next.textContent ="終了";
         }else if(!((conten)> 0)){
             returnn.style.color = ("var(--bu-invalid-color)");
             returnn.style.borderColor = ("var(--bu-invalid-color)");
@@ -133,6 +141,8 @@
         if((conten +1)< setting.query){
             conten++;
             update(conten);
+        }else{
+            end();
         }
     });
 
@@ -145,11 +155,42 @@
 
     });
 
+    //end
+    eend.addEventListener("click",() =>{
+        alert("未実装です。")
+    })
+
 
     //回答確認
     answer.addEventListener('click',() => {
-        anser(Number(user_input.value));
+        if(user_input.value !== ""){
+            anser(Number(user_input.value));
+        };
     });
+
+    user_input.addEventListener("input",() => {
+        console.log("aaaaaaaaaaaa")
+        if(user_input.value.trim() === ''){
+            answer.style.color = ("var(--bu-invalid-color)");
+            answer.style.borderColor = ("var(--bu-invalid-color)");
+        }else{
+            answer.style.color = ("var(--bu-answer-color)");
+            answer.style.borderColor = ("var(--bu-answer-color)");
+        };
+    });
+
+    //メニュー
+    menu.addEventListener("click",()=>{
+        alert("未実装です。")
+    });
+
+
+
+    
+
+    function end(){
+        alert("未実装です。")
+    }
 
     //回答保存
     user_input.addEventListener('input',()=>{
