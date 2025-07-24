c5638fb0cb84bdf3cb12cce153f5aeb34424ac9f
komekome12 <ludouzhongze5@gmail.com>
2025-07-24 15:32:12 +0900

    エラー修正

--- src/js/solving.js
+++ src/js/solving.js
@@ -16,8 +16,10 @@
 
 //Start
 const data = set()
-const data_list = data_set(data.key_name,data.request)
-data_set(data_list)
+const data_query = data.request;
+const data_key = data.key_name;
+console.log(`【デバッグ】データ:${data_key}:${data_query}`);
+const data_list = data_set(data_key,data_query)
 
 //関数達！！↑コードがかわいいいねえええええええええ
 //だいすきいいいいいいい可愛い子供ねええねええええ
@@ -26,15 +28,17 @@ function set(){
     const parment = new URLSearchParams(window.location.search);
 
     const key_name = parment.get('key_name')
-    const request = parment.get('request')
+    const request = ((parment.get('request') ?? "").split(",").map(Number));
 
     const save = {
         key_name:key_name ?? null,
         request:request ?? null,
     }
+    console.log(`【デバッグ】セーブ:${save}`);
+    console.log(save);
+
     return(save);
 }
-console.log(data);
 
 function data_set(key,query){
 
@@ -42,25 +46,36 @@ function data_set(key,query){
     const data2 = JSON.parse(data1);
     const data3 = data2 ?? null;
 
-    const data_two1 = data3.Problem_Content;//問題の内容
-    const data_three1 = data3.User_Answers;//ユーザーの入力
-    const data_four1 = data3.Problem_Answer;//問題の答え
+
+    const data_two1 = data3?.Problem_Content ?? null;//問題の内容
+    const data_three1 = data3?.User_Answers ?? null;//ユーザーの入力
+    const data_four1 = data3?.Problem_Answer ?? null;//問題の答え
 
     const data_two2 = [];
     const data_three2 = [];
     const data_four2 = [];
 
-    query.forEach((query,i)=>{
-        
-        const b = query[i]
-        const content1 = data_two1[b];//問題の内容
-        const content2 = data_three1[b];//ユーザーの入力
-        const content3 = data_four1[b];//問題の答え
+    query.forEach((query,i) => {
+        console.log(`【デバッグ】${i}回目のクエリ:${query}`)
 
-        data_two2.push(content1)
-        data_three2.push(content2)
-        data_four2.push(content3)
-        
+        const content1 = data_two1[query];//問題の内容
+        const content2 = data_three1[query];//ユーザーの入力
+        const content3 = data_four1[query];//問題の答え
+
+        data_two2.push(content1)//問題の内容
+        data_three2.push(content2)//ユーザーの入力
+        data_four2.push(content3)//問題の答え
+
     });
+    const data_set = {
+        Problem_Content:data_two2, //問題の内容
+        User_Answers:data_three2, //ユーザーの入力
+        Problem_Answer:data_four2, //問題の答え
+    };
 
-}
+        console.log("===<データ>===")
+        console.log(data_set)
+
+    return (data_set)
+
+    };
