b261d7dd76da3d37dbfbec40a51078d99b58538d
komekome12 <ludouzhongze5@gmail.com>
2025-08-20 09:04:08 +0900

    aaaaaaa

--- src/common/js/color-mode.js
+++ src/common/js/color-mode.js
@@ -1,18 +1,24 @@
+const mode_color = window.matchMedia('(prefers-color-scheme: dark)')
 const mode = document.getElementById("color-mode")
 
-let mode_name = "drak"
+let mode_name = "auto"
 
-
-if(mode_name === "drak"){
-    mode.classList.add("mode--dark")
-}else if(mode_name === "light"){
-    mode.classList.remove("mode--dark")
-}else if(mode_name === "auto"){
-    const modes = auto();
-};
-
+mode_color.addEventListener("change", auto);
+auto()
 function auto(){
-let mode_name = aaa;
-
-    return(mode_name)
+    if(mode_name === "drak"){
+        mode.classList.add("mode--dark")
+    }else if(mode_name === "light"){
+        mode.classList.remove("mode--dark")
+    }else if(mode_name === "auto"){
+        const auto_vale = mode_color.matches;
+        console.log(auto_vale)
+        if(auto_vale){
+            mode.classList.add("mode--dark")
+            console.log("auto--dark")
+        }else{
+            mode.classList.remove("mode--dark")
+            console.log("auto--light")
+        }
+    };
 }
